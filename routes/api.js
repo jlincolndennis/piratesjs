bbe5e56a8d609d3c49c419dc44b3cbf6240d3017
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-05-09 17:44:42 -0600

    back end database call works

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var knex = require('knex')(require('../knexfile')["development"]);
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  knex('pirates')
+router.get('/pirates', function(req, res, next) {
+  knex('pirate_table').then(function (data){
+    res.status(400).json(data)
+  })
 });
 
 module.exports = router;
